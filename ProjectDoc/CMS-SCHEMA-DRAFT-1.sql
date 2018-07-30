903f1050e3be95008a5ab7b6e7ea3bd39a3148bd
OSSYULYYZ <yimeng.zhu@mail.mcgill.ca>
2018-07-29 21:34:36 -0400

    Last Foreign Key Commented Out. Everything else is working

--- ProjectDoc/CMS-SCHEMA-DRAFT-1.sql
+++ ProjectDoc/CMS-SCHEMA-DRAFT-1.sql
@@ -12,7 +12,7 @@ CREATE TABLE account (
   middle_initial TEXT NOT NULL DEFAULT '',
   last_name TEXT NOT NULL,
   email TEXT NOT NULL,
-  phone TEXT NOT NULL,
+  phone TEXT NOT NULL
 );
 
 CREATE TABLE account_login (
@@ -20,17 +20,6 @@ CREATE TABLE account_login (
   username TEXT NOT NULL,
   password TEXT NOT NULL,
   CONSTRAINT login_id FOREIGN KEY (id) REFERENCES account (id)
-);
-
--- EMPLOYEES --
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  insurance_plan VARCHAR(32) NOT NULL,
-  role VARCHAR(32) NOT NULL,
-  weekly_hours INTEGER NOT NULL,
-  CONSTRAINT employee_account FOREIGN KEY (id) REFERENCES account (id),
-  CONSTRAINT employee_insurance FOREIGN KEY (insurance_plan) REFERENCES insurance_plan (name),
-  CONSTRAINT employee_role FOREIGN KEY (role) REFERENCES role (name)
 );
 
 /** Sales associate, Manager, employee, Admin **/
@@ -44,20 +33,55 @@ CREATE TABLE insurance_plan (
   pct_reimbursed INTEGER NOT NULL
 );
 
-/** Indicates what employees want to work on (why does this company let them choose) **/
-CREATE TABLE work_choice (
-  employee INTEGER NOT NULL,
-  contract_type VARCHAR(32) NOT NULL,
-  platform VARCHAR(32) NOT NULL,
-  PRIMARY KEY (employee, contract_type, platform),
-  CONSTRAINT choice_employee FOREIGN KEY (employee) REFERENCES employee (id),
-  CONSTRAINT choice_contract FOREIGN KEY (contract_type) REFERENCES contract_type (name),
-  CONSTRAINT choice_platform FOREIGN KEY (platform) REFERENCES platform (name)
+-- EMPLOYEES --
+CREATE TABLE employee (
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  insurance_plan VARCHAR(32) NOT NULL,
+  role VARCHAR(32) NOT NULL,
+  weekly_hours INTEGER NOT NULL,
+  CONSTRAINT employee_account FOREIGN KEY (id) REFERENCES account (id),
+  CONSTRAINT employee_insurance FOREIGN KEY (insurance_plan) REFERENCES insurance_plan (name),
+  CONSTRAINT employee_role FOREIGN KEY (role) REFERENCES role (name)
+);
+
+/* Gold, Premium, Silver, ... */
+CREATE TABLE contract_type (
+  name VARCHAR(32) PRIMARY KEY NOT NULL
+);
+
+
+CREATE TABLE line_of_business (
+  name VARCHAR(32) PRIMARY KEY NOT NULL
+);
+
+/* Think he means Cloud, On-Prem */
+CREATE TABLE platform (
+  name VARCHAR(32) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE city (
+  city VARCHAR(16) NOT NULL,
+  province ENUM('BC','AB','SK','MB','ON','QC','NB','NS','PE','DD','NL','YT','NT','NU') NOT NULL,
+  PRIMARY KEY (city, province)
+);
+
+-- CLIENTS --
+/** Clients sign up for contracts, here we store their details **/
+CREATE TABLE client (
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  client_rep INTEGER NOT NULL, /** This is the account # **/
+  company_name TEXT NOT NULL,
+  address TEXT NOT NULL,
+  city VARCHAR(16) NOT NULL,
+  province ENUM('BC','AB','SK','MB','ON','QC','NB','NS','PE','DD','NL','YT','NT','NU') NOT NULL,
+  postal_code CHAR(7) NOT NULL,
+  CONSTRAINT FOREIGN KEY (client_rep) REFERENCES account (id),
+  CONSTRAINT FOREIGN KEY (city, province) REFERENCES city (city, province)
 );
 
 -- CONTRACTS --
 CREATE TABLE contract (
-  id INTEGER PRIMARY NOT NULL AUTO_INCREMENT,
+  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   client INTEGER NOT NULL,
   line_of_business VARCHAR(32) NOT NULL,
   contract_type VARCHAR(32) NOT NULL,
@@ -70,7 +94,18 @@ CREATE TABLE contract (
   CONSTRAINT contract_lob  FOREIGN KEY (line_of_business) REFERENCES line_of_business (name),
   CONSTRAINT contract_ctype FOREIGN KEY (contract_type) REFERENCES contract_type (name),
   CONSTRAINT contract_platform FOREIGN KEY (platform) REFERENCES platform (name),
-  CONSTRAINT contract_manager FOREIGN KEY (manager) REFERENCES employee (id),
+  CONSTRAINT contract_manager FOREIGN KEY (manager) REFERENCES employee (id)
+);
+
+/** Indicates what employees want to work on (why does this company let them choose) **/
+CREATE TABLE work_choice (
+  employee INTEGER NOT NULL,
+  contract_type VARCHAR(32) NOT NULL,
+  platform VARCHAR(32) NOT NULL,
+  PRIMARY KEY (employee, contract_type, platform),
+  CONSTRAINT choice_employee FOREIGN KEY (employee) REFERENCES employee (id),
+  CONSTRAINT choice_contract FOREIGN KEY (contract_type) REFERENCES contract_type (name),
+  CONSTRAINT choice_platform FOREIGN KEY (platform) REFERENCES platform (name)
 );
 
 CREATE TABLE sale_record (
@@ -82,25 +117,11 @@ CREATE TABLE sale_record (
   CONSTRAINT sale_associate FOREIGN KEY (sales_associate) REFERENCES employee (id)
 );
 
-/* Gold, Premium, Silver, ... */
-CREATE TABLE contract_type (
-  name VARCHAR(32) PRIMARY KEY NOT NULL
-);
-
-/* Think he means Cloud, On-Prem */
-CREATE TABLE platform (
-  name VARCHAR(32) PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE line_of_business (
-  name VARCHAR(32) PRIMARY KEY NOT NULL
-);
-
 CREATE TABLE deliverable_standard (
   contract_type VARCHAR(32) NOT NULL,
   deliverable VARCHAR(32) NOT NULL,
   due_after INTEGER NOT NULL,
-  PRIMARY_KEY (contract_type, deliverable),
+  PRIMARY KEY (contract_type, deliverable),
   CONSTRAINT deliverable_contract_type FOREIGN KEY (contract_type) REFERENCES contract_type (name)
 );
 /** Used by employees to track hours **/
@@ -109,8 +130,8 @@ CREATE TABLE contract_assignment (
   employee INTEGER NOT NULL,
   hours_worked INTEGER NOT NULL DEFAULT 0,
   PRIMARY KEY (contract, employee),
-  FOREIGN KEY asg_contract FOREIGN KEY (contract) REFERENCES contract (id),
-  FOREIGN KEY asg_employee FOREIGN KEY (employee) REFERENCES employee (id)
+  FOREIGN KEY (contract) REFERENCES contract (id),
+  FOREIGN KEY (employee) REFERENCES employee (id)
 );
 
 /** Used to determine if deliverables are on time **/
@@ -118,29 +139,8 @@ CREATE TABLE deliverable_log (
   contract INTEGER NOT NULL,
   deliverable VARCHAR(32) NOT NULL,
   completed_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (contract, deliverable),
-  -- We should be able to do a constraint on contract_type + deliverable name, but this might violate NFs
-  CONSTRAINT log_deliverable_standard FOREIGN KEY (deliverable) REFERENCES deliverable_standard (deliverable)
-);
-
--- CLIENTS --
-/** Clients sign up for contracts, here we store their details **/
-CREATE TABLE client (
-  id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  client_rep INTEGER NOT NULL, /** This is the account # **/
-  company_name TEXT NOT NULL,
-  address TEXT NOT NULL,
-  city TEXT NOT NULL,
-  province ENUM('BC','AB','SK','MB','ON','QC','NB','NS','PE','DD','NL','YT','NT','NU') NOT NULL,
-  postal_code CHAR(7) NOT NULL,
-  CONSTRAINT client_rep_account FOREIGN KEY client_rep REFERENCES account (id),
-  CONSTRAINT client_valid_city FOREIGN KEY (city,province) REFERENCES city (city,province)
-);
-
-CREATE TABLE city (
-  city VARCHAR(16) NOT NULL,
-  province ENUM('BC','AB','SK','MB','ON','QC','NB','NS','PE','DD','NL','YT','NT','NU') NOT NULL,
-  PRIMARY KEY (city, province)
+  PRIMARY KEY (contract, deliverable) -- ,
+  -- CONSTRAINT FOREIGN KEY (deliverable) REFERENCES deliverable_standard (deliverable)
 );
 
 SET FOREIGN_KEY_CHECKS=1;
---
+++ readme.md
@@ -0,0 +1,4 @@
+- Put new pages in the 'pages' folder
+- The CSS should have the same name as the page
+- Link to pages: "index.php?page=myPage" (So the header is automatically set, the JS files included, the CSS file too, see index.php for more information)
+- Interface the database through TDGs (Avoids spaghetti code for interfacing the database)

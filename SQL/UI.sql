69624847d5d6c6a72127b4788ef6a67ec93b8366
OSSYULYYZ <yimeng.zhu@mail.mcgill.ca>
2018-07-31 12:47:05 -0400

    MOST QUERIES DONE

--- SQL/UI.sql
+++ SQL/UI.sql
@@ -1,18 +1,11 @@
--- 1 --
-SELECT client
-FROM contract
-WHERE COUNT(contract) = 1
-
+-- 1 HAVING TROUBLE GETTING THE MOST OF EACH CATEGORY --
 -- 2 --
 SELECT * FROM contract
 WHERE start_date > ADDDATE(NOW(), -10)
-
 -- 3 --
 SELECT * FROM employee
 WHERE jurisdiction = 'QC';
-
 -- 4 --
 SELECT id FROM contract
 where contract_type = 'Gold';
-
--- 5 -- 
+-- 5 NEED TO DO REPORT -- 

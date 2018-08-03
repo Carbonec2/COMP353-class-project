6a6f5ac813408f7009bd6229b4fc784ec7290e00
Jean-Michel Laliberté <jmfreedom@gmail.com>
2018-08-03 16:55:02 -0400

    Removed external unused font

--- includes/head.php
+++ includes/head.php
@@ -1,11 +1,7 @@
 
 <!-- THIS IS FOR BOOTSTRAP -->
-
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="css/lib/bootstrap-3.3.7/dist/css/bootstrap.min.css">
 <!-- Latest compiled JavaScript -->
 <script src="css/lib/bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
-
-<!-- Font -->
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <!-- END FOR BOOTSTRAP-->

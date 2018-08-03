bca0c85677de68fec247b264eb588e1602d00499
Jean-Michel Laliberté <jmfreedom@gmail.com>
2018-08-03 17:14:19 -0400

    Some feedback for the account creation

--- js/controller/createClientAccount.js
+++ js/controller/createClientAccount.js
@@ -15,8 +15,8 @@ class CreateClientAccount {
 
         this.bind();
         this.selectFill();
-        
-        $("#city").attr('disabled',true);//Enabled only when we select a province, see selectFill member method
+
+        $("#city").attr('disabled', true);//Enabled only when we select a province, see selectFill member method
 
     }
 
@@ -40,23 +40,23 @@ class CreateClientAccount {
             //Fill choices
             let provinces = Object.keys(result);
 
-            provinces.forEach((entry)=>{
+            provinces.forEach((entry) => {
                 $("#province").append($("<option />").val(entry).text(entry));
             });
-            
-            $("#province").change(()=>{
+
+            $("#province").change(() => {
                 $("#city").empty();
-                
+
                 //We append the set of cities from the selected province
                 let cities = result[$("#province").val()];
-                
-                cities.forEach((entry)=>{
+
+                cities.forEach((entry) => {
                     $("#city").append($("<option />").val(entry).text(entry));
                 });
-                
-                $("#city").attr('disabled',false);
+
+                $("#city").attr('disabled', false);
             });
-            
+
             //We trigger it because there is a default value set above
             $("#province").trigger('change');
         });
@@ -71,15 +71,15 @@ class CreateClientAccount {
                     return this.id;
                 }) // convert to set of IDs
                 .get(); // convert to instance of Array (optional)
-        
+
         let selectAttributes = $("#clientAccountForm select[id]")         // find spans with ID attribute
                 .map(function () {
                     return this.id;
                 }) // convert to set of IDs
                 .get();
-        
+
         formAttributes = formAttributes.concat(selectAttributes);
-        
+
 
         //We take every attribute and set the corresponding value of the object member
         formAttributes.forEach((id) =>
@@ -98,9 +98,17 @@ class CreateClientAccount {
 
 
         clientTDG.saveClientAndAccount(data, (result) => {
-            if (typeof (result) !== "undefined" && result !== false && typeof (result.id) !== "undefined" && parseInt(result.id) !== 0) {
-                console.log("Authentication successful");
-            }
+            
+            //We should redirect to a general panel, but we will do it later on
+
+            $("#clientAccountForm").empty();
+
+            let $p = $('<p></p>');
+            $p.append('Account created with success. Client number: ' + result);
+
+            $("#clientAccountForm").append($p);
+            
+            $("#formSubmit").delete();
 
             console.log(result);
         });

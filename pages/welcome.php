727beb57e67e58b3b8f4a01ad49fea787c18cf69
OSSYULYYZ <yimeng.zhu@mail.mcgill.ca>
2018-08-12 18:12:42 -0400

    Updated Welcome Page

--- pages/welcome.php
+++ pages/welcome.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <p>The quick brown fox jumps over the lazy dog</p>
 
 <p>The quick brown fox jumps over the lazy dog2</p>
@@ -21,3 +22,7 @@
 
 <p>The quick brown fox jumps over the lazy dog10</p>
 
+=======
+<h1>AT YOUR SERVICE</h1>
+<img src="dcma.jpg">
+>>>>>>> 4e097ae... Updated Welcome Page
